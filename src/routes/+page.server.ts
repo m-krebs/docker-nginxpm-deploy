af375451b405b3c7893f49cb4b290de907284d17
Michael Krebs <michikrbs@gmail.com>
2024-12-07 13:37:27 +0100

    refactor: change command execution logic

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,22 +1,9 @@
-import { error } from '@sveltejs/kit';
-import { exec } from 'child_process'
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
-	const command = 'docker ps -a';
-	const commandResult = await new Promise<string>((resolve, reject) => {
-		exec(command, (err, stdout, stderr) => {
-			if (err || stderr) {
-				reject(err || stderr);
-			} else {
-				resolve(stdout.trim())
-			}
-		})
-	}).catch((err) => {
-		throw error(500, `Command failed: ${err}`);
-	});
+import { executeCmdSubProcess } from '$lib/utils';
 
+export const load: PageServerLoad = async () => {
 	return {
-		commandResult
+		commandResult: executeCmdSubProcess("ls"),
 	}
 };
